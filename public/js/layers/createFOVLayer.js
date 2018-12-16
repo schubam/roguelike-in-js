83b5721aae6c6784a8d7dfe13d940ac849eecb58
Marc Schuba <me@schubam.com>
2018-12-16 13:41:16 +0100

    doesnt work

--- public/js/layers/createFOVLayer.js
+++ public/js/layers/createFOVLayer.js
@@ -2,32 +2,52 @@ import { createBuffer, TILE_SIZE, renderPaletteTile } from "../render.js";
 import loadColorPalette from "../loaders/loadColorPalette.js";
 import { isWall } from "../tryMovePlayer.js";
 
+const visible = Symbol("visible");
+const nonVisible = Symbol("non-visible");
+
 function scanline(rowY, row, playerX, playerY) {
   const viewMap = new Array(row.length);
-  const visible = Symbol("visible");
-  const nonVisible = Symbol("non-visible");
+  const yDistance = Math.abs(rowY - playerY);
+  if (yDistance > 3) {
+    for (let i = 0; i < viewMap.length; i++) {
+      viewMap[i] = nonVisible;
+    }
+    return viewMap;
+  } else if (yDistance === 3) {
+    for (let i = 0; i < viewMap.length; i++) {
+      viewMap[i] = nonVisible;
+    }
+    return viewMap;
+  } else if (yDistance === 2) {
+    for (let i = 0; i < viewMap.length; i++) {
+      viewMap[i] = nonVisible;
+    }
+    return viewMap;
+  } else if (yDistance === 1) {
+    for (let i = 0; i < viewMap.length; i++) {
+      viewMap[i] = nonVisible;
+    }
+    return viewMap;
+  } else if (yDistance === 0) {
+    // rowY === playerY
+    const left = row.slice(0, playerX).reverse();
+    const right = row.slice(playerX + 1);
 
-  if (Math.abs(rowY - playerY) > 3) {
-    return new Array(row.length).map(i => nonVisible);
-  }
+    const ret = [left, right].map(tiles => {
+      let foundWall = 0;
+      return tiles.map(tile => {
+        if (foundWall > 0) {
+          return nonVisible;
+        }
 
-  const left = row.slice(0, playerIdx).reverse();
-  const right = row.slice(playerIdx + 1);
-
-  const ret = [left, right].map(tiles => {
-    let foundWall = 0;
-    return tiles.map(tile => {
-      if (foundWall > 0) {
-        return nonVisible;
-      }
-
-      if (isWall(tile)) {
-        foundWall++;
-      }
-      return visible;
+        if (isWall(tile)) {
+          foundWall++;
+        }
+        return visible;
+      });
     });
-  });
-  return [...ret[0].reverse(), visible, ...ret[1]];
+    return [...ret[0].reverse(), visible, ...ret[1]];
+  }
 }
 
 export async function createFOVLayer(camera, levelStore) {
@@ -40,13 +60,14 @@ export async function createFOVLayer(camera, levelStore) {
   context.clearRect(0, 0, buffer.width, buffer.height);
 
   const x = camera.pos.x;
-  let y = player.position.y;
+  let y = Math.floor(camera.pos.y / TILE_SIZE);
   const lines = [];
   do {
+    console.log(y);
     const row = grid.getRow(x, y, Math.floor(camera.size.x / TILE_SIZE));
     lines.push(
       scanline(
-        y,
+        Math.floor(y / TILE_SIZE),
         row,
         player.position.x + Math.floor(camera.pos.x / TILE_SIZE),
         player.position.y + Math.floor(camera.pos.y / TILE_SIZE)
@@ -54,19 +75,16 @@ export async function createFOVLayer(camera, levelStore) {
     );
     y += TILE_SIZE;
   } while (y < camera.pos.y + camera.size.y);
-  // if (
-  //   !visible(
-  //     player.position,
-  //     { x: Math.floor(x / TILE_SIZE), y: Math.floor(x / TILE_SIZE) },
-  //     grid
-  //   )
-  // ) {
-  //   renderPaletteTile("W", x, y, context, palette);
-  // } else {
-  //   console.log("vis");
-  // }
-  // }
-  // }
+  console.log(lines);
+  lines.forEach((line, y) => {
+    line.forEach((tile, x) => {
+      if (tile === visible) {
+        console.log("vis");
+      } else {
+        renderPaletteTile("W", x, y, context, palette);
+      }
+    });
+  });
 
   return function(context, camera) {
     context.drawImage(
--- todo.md
+++ todo.md
@@ -3,6 +3,7 @@
 ## Worked on last
 
 - update UI with entity changes after collision (right now after state update gold disappears, but player not there yet)
+- use hero animations that are bigger than 16x16
 - show enemies
 - make rooms/maps procedurally
   - use patterns for ground
