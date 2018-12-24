8c412a8656e555c11f38b48f46e7dc0d0f239856
Marc Schuba <me@schubam.com>
2018-12-24 14:02:55 +0100

    don't render entities off-screen

--- public/js/layers/createSpriteLayer.js
+++ public/js/layers/createSpriteLayer.js
@@ -1,5 +1,17 @@
 import { TILE_SIZE } from "../render.js";
 
+function onScreen(pos, camera) {
+  const left = camera.pos.x;
+  const right = camera.pos.x + camera.size.x;
+  const up = camera.pos.y;
+  const down = camera.pos.y + camera.size.y;
+
+  if (pos.x < left || pos.x > right || pos.y < up || pos.y > down) {
+    return false;
+  }
+  return true;
+}
+
 export default function createSpriteLayer(entities, width, height) {
   const spriteBuffer = document.createElement("canvas");
   spriteBuffer.width = width;
@@ -14,7 +26,9 @@ export default function createSpriteLayer(entities, width, height) {
         x: entity.pos.x - camera.pos.x,
         y: entity.pos.y - camera.pos.y + TILE_SIZE
       };
-      context.drawImage(spriteBuffer, pos.x, pos.y);
+      if (onScreen(pos, camera)) {
+        context.drawImage(spriteBuffer, pos.x, pos.y);
+      }
     });
   };
 }
